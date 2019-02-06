4ce52dbce1776b8ad485d3bab21b41c117e3d6fa
Vyacheslav <amcishow@gmail.com>
2019-02-06 04:46:23 +0300

    feature: add appcenter config

--- app/src/main/java/com/amciof/Yandex_MDS2019/MainActivity.java
+++ app/src/main/java/com/amciof/Yandex_MDS2019/MainActivity.java
@@ -1,16 +1,30 @@
 package com.amciof.Yandex_MDS2019;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        Fabric.with(this, new Crashlytics());
+        AppCenter.start(getApplication(), "4f91900c-d92f-4bd0-be30-658a50f1e1bf", Analytics.class, Crashes.class);
     }
 }
